b5580046691fc9c385359648288d5cc575b20061
kostanzhoglo <jeffreywithers@gmail.com>
2018-11-09 17:19:09 -0500

    Added fs.readfile to appTraversy, which will read from index.html, also created

--- appTraversy.js
+++ appTraversy.js
@@ -1,16 +1,24 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3333;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-type', 'text/plain');
-  // next line will be output to the browser.
-  res.end('Yo dude');
-});
+fs.readFile('index.html', (err, html) => {
+  if (err) {
+    throw err;
+  }
 
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/html');
+    // next line will be output to the browser.
+    res.write(html);
+    res.end();
+  });
 
-server.listen(port, hostname, () => {
-  console.log(`Server started on port ${port}`)
+  server.listen(port, hostname, () => {
+    console.log(`Server started on port ${port}`)
+  });
+
 });
